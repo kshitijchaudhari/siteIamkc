57c993ed0dcaf4d382d5a093bf8fd04c8bc7e7da
kshitijchaudhari <kshitijchaudhari@outlook.com>
2019-04-23 23:19:04 +0530

    First commit with files

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#sideNav'
+    });
+
+    $('#emailBtn').click(function() {
+        $('#emailBtn').attr("disabled", true);
+        $('#emailBtn').text("Wait");
+
+
+    });
+
+})(jQuery); // End of use strict
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("totop").style.display = "block";
+    } else {
+        document.getElementById("totop").style.display = "none";
+    }
+}
